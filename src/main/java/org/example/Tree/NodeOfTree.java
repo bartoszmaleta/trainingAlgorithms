85eba68dbad5c8b607f8747de3718d08955af2dc
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-08-31 16:57:09 +0200

    Add implementation of Tree

---
+++ src/main/java/org/example/Tree/NodeOfTree.java
@@ -0,0 +1,54 @@
+package org.example.Tree;
+
+public class NodeOfTree {
+    NodeOfTree left, right;
+    int data;
+    public NodeOfTree(int data) {
+        this.data = data;
+    }
+
+    public void insert(int value) {
+        if (value <= data) {
+            if (left == null) {
+                left = new NodeOfTree(value);
+            } else {
+                left.insert(value);
+            }
+        } else {
+            if (right == null) {
+                right = new NodeOfTree(value);
+            } else {
+                right.insert(value);
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        if (value == data) {
+            return true;
+        } else if (value < data) {
+            if (left == null) {
+                return false;
+            } else {
+                return left.contains(value);
+            }
+        } else {
+            if (right == null) {
+                return false;
+            } else {
+                return right.contains(value);
+            }
+        }
+    }
+
+    public void printInOrder() {
+        if (left != null) {
+            left.printInOrder();
+        }
+        System.out.println(data);
+        if (right != null) {
+            right.printInOrder();
+        }
+    }
+
+}
