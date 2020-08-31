374ef24d3001e059f3947b9dfd0d3f3f3072ef1d
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-08-31 17:42:22 +0200

    Add implemenation of Queue

---
+++ src/main/java/org/example/Queue.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Queue {
+    private static class NodeOfQueue {
+        private final int data;
+        private NodeOfQueue next;
+        private NodeOfQueue(int data) {
+            this.data = data;
+        }
+    }
+
+    private NodeOfQueue head;
+    private NodeOfQueue tail;
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int peek() {
+        return head.data;
+    }
+
+    public void add(int data) {
+        NodeOfQueue nodeOfQueue = new NodeOfQueue(data);
+        if (tail != null) {
+            tail.next = nodeOfQueue;
+        }
+        tail = nodeOfQueue;
+        if (head == null) {
+            head = nodeOfQueue;
+        }
+    }
+
+    public int remove() {
+        int data = head.data;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        return data;
+    }
+}
